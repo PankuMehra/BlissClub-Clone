52f89f248ce058bbb3a8f66ade6c22ca15a6afae
kajalyadavgithub <kajalyadav20020411@gmail.com>
2022-10-14 00:29:39 +0530

    task

---
+++ footer/footer-exp.js
@@ -0,0 +1,62 @@
+let footer = () => {
+    return `<div class="container">
+    <div class="row">
+        <div class="footer-col">
+            <h4>ABOUT US</h4>
+            <ul>
+                <li><a href="#">Our Story</a></li>
+                <li><a href="#">Careers</a></li>
+                <li><a href="#">WWM blog</a></li>
+                <li><a href="#">Terms & Services</a></li>
+                <li><a href="#">Privacy Policy</a></li>
+            </ul>
+        </div>
+        <div class="footer-col">
+            <h4>HELP</h4>
+            <ul>
+                <li><a href="#">Order Tracking</a></li>
+                <li><a href="#">Returns / Refunds / Exchanges</a></li>
+                <li><a href="#">Shipping</a></li>
+                <li><a href="#">FAQ</a></li>
+                <li><a href="#">Contact Us</a></li>
+            </ul>
+        </div>
+        <div class="footer-col">
+            <h4>REWARDS & BENEFITS</h4>
+            <ul>
+                <li><a href="#">BlissQueen Rewards Program</a></li>
+                <li><a href="#">Refer & Earn</a></li>
+                <li><a href="#">MissBliss Affiliate Program</a></li>
+            </ul>
+        </div>
+        <div class="footer-col">
+            <h4>ADDRESS</h4>
+            <ul>
+                <li><a href="#">BlissClub Fitness Private Limited
+                        No. 2908, Club Road, 22nd Cross Rd, 2nd Sector,
+                        Parangi Palaya, UrbanVault, HSR Layout,
+                        Bengaluru, Karnataka 560034
+                        CIN - U52520KA2020PTC133220
+                        In case of queries email us at :
+                        grievanceofficer@myblissclub.in</a></li>
+            </ul>
+        </div>
+        <div class="footer-col1">
+            <div class="social-links">
+                <a href="https://www.facebook.com/myblissclub"><i class="fab fa-2x fa-facebook-f"></i></a>
+                <a href="https://twitter.com/myblissclub"><i class="fab fa-2x  fa-twitter"></i></a>
+                <a href="https://www.instagram.com/myblissclub/"><i class="fab fa-2x fa-instagram"></i></a>
+                <a href="https://www.linkedin.com/company/my-bliss-club/"><i class="fab fa-2x  fa-linkedin-in"></i></a>
+            </div>
+        </div>
+        <!-- <div class="bottom-links">
+            <a href="#"></a>
+            <a href="#"></a>
+            <a href="#"></a>
+            <a href="#"></a>
+        </div> -->
+    </div>
+</div>`
+}
+ 
+export{footer};
