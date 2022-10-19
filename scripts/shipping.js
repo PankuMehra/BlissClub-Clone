cb84260df687a380f8bc4a1f21ef1593f7088e8c
Pankaj_Mehra <meenakshi9991833@gmail.com>
2022-10-19 17:58:22 +0530

    coupon Applied

--- scripts/shipping.js
+++ scripts/shipping.js
@@ -1,26 +1,25 @@
+import {
+  checkout_cart,
+  checkout_cart1,
+  displayitems,
+  displayitems1,
+  shipping_item,
+} from "../utils/checkout_cart.js";
 
- import { checkout_cart,checkout_cart1, displayitems,displayitems1, shipping_item } from "../utils/checkout_cart.js";
-
- document.getElementById("checkout_mid2").innerHTML = checkout_cart();
-
+document.getElementById("checkout_mid2").innerHTML = checkout_cart();
 
 document.querySelector("#checkout_mid_hidden").innerHTML = checkout_cart1();
 
 //  import { displayitems } from "./checkout.js";
 
-let allitem = JSON.parse(localStorage.getItem("cart_items"))||[] ;
+let allitem = JSON.parse(localStorage.getItem("cart_items")) || [];
 console.log(allitem);
 
- displayitems(allitem);
+displayitems(allitem);
 
- displayitems1(allitem);
-
+displayitems1(allitem);
 
 //  document.querySelector(".checkout_adress_div").innerHTML=contact_adress_html();
-
- 
-
-
 
 // console.log("ram");
 
@@ -30,8 +29,80 @@ console.log(allitem);
 //     let email1=document.querySelector(".shipping_email").innerText=adress.email;
 //     let final_adress=`${adress.adress1},${adress.apartment},${adress.city},${adress.state},${adress.pincode}`;
 //     let all_adress=document.querySelector(".shipping_adress").innerText=final_adress;
-    
+
 //     // shipping_email.innerText=email;
 //     console.log(adress);
 //  }
- shipping_item();
+
+let CartTotalPrice = localStorage.getItem("cart_total_price");
+document.getElementById("total_price_span1").innerText = CartTotalPrice;
+document.getElementById("total_price_span2").innerText = CartTotalPrice;
+
+shipping_item();
+
+let checkCoupon = () => {
+  console.log("checkCoupon Called");
+  let couponValue = document.getElementById("couponInput1").value;
+  let myCoupons = JSON.parse(localStorage.getItem("coupens")) || [];
+  let CartTotalPrice = localStorage.getItem("cart_total_price") || 0;
+  let couponApplied = localStorage.getItem("couponApplied") || false;
+  let couponFound = false;
+  if (couponApplied) {
+    console.log(couponApplied);
+    myCoupons.map((elem) => {
+      if (elem == couponValue) {
+        couponFound = true;
+      }
+    });
+    if (couponFound == false) {
+      document.getElementById(
+        "successfullAlert"
+      ).innerText = `Wrong Coupon Code`;
+      document.getElementById("successfullAlert").style.backgroundColor = "red";
+      document.getElementById("successfullAlert").style.display = "flex";
+      setTimeout(function () {
+        document.getElementById("successfullAlert").style.display = "none";
+      }, 1350);
+    } else {
+      if (couponValue == "bliss10") {
+        CartTotalPrice = Math.round(CartTotalPrice * (9 / 10));
+      } else if (couponValue == "bliss20") {
+        CartTotalPrice = Math.round(CartTotalPrice * (8 / 10));
+      } else if (couponValue == "bliss30") {
+        CartTotalPrice = Math.round(CartTotalPrice * (7 / 10));
+      } else if (couponValue == "bliss40") {
+        CartTotalPrice = Math.round(CartTotalPrice * (6 / 10));
+      }
+
+      document.getElementById("successfullAlert").innerText = `Coupon Applied`;
+      document.getElementById("successfullAlert").style.backgroundColor =
+        "rgb(30, 172, 30)";
+      document.getElementById("successfullAlert").style.display = "flex";
+      setTimeout(function () {
+        document.getElementById("successfullAlert").style.display = "none";
+      }, 1350);
+
+      document.getElementById("total_price_span1").innerText = CartTotalPrice;
+      document.getElementById("total_price_span2").innerText = CartTotalPrice;
+      localStorage.setItem("cart_total_price", CartTotalPrice);
+      localStorage.setItem("couponApplied", true);
+    }
+  } else {
+    document.getElementById(
+      "successfullAlert"
+    ).innerText = `Coupon already Applied`;
+    document.getElementById("successfullAlert").style.backgroundColor = "red";
+    document.getElementById("successfullAlert").style.display = "flex";
+    setTimeout(function () {
+      document.getElementById("successfullAlert").style.display = "none";
+    }, 1350);
+  }
+  // document.getElementById("couponSubtotal1").innerText = CartTotalPrice;
+};
+
+document
+  .getElementById("checkoutCouponButton1")
+  .addEventListener("click", checkCoupon);
+document
+  .getElementById("checkoutCouponButton2")
+  .addEventListener("click", checkCoupon);
