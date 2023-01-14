fe86bf16e37dbbf812e70b7ad39f55ce535228a4
Pankaj_Mehra <meenakshi9991833@gmail.com>
2023-01-14 20:59:15 +0530

    URL with glitch

--- URL/URL.js
+++ URL/URL.js
@@ -1,5 +1,6 @@
 const URL = () => {
-  const data = "http://localhost:3001/data";
+  // const data = "http://localhost:3001/data";
+  const data = "https://jumpy-cultured-william.glitch.me/data";
   return data;
 };
 export default URL();
